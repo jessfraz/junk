663dc7306192590e0beb4404fd00291fbb9df6cd
Kris Nova <kris@nivenly.com>
2018-03-29 15:49:40 -0700

    Adding totessafe scaffolding

---
+++ totessafe/reflector/external_server.go
@@ -0,0 +1,32 @@
+package reflector
+
+import (
+	//"github.com/kris-nova/terraformctl/parser"
+	//"github.com/kris-nova/terraformctl/storage"
+	"golang.org/x/net/context"
+)
+
+var _ ReflectorExternalServer = ExternalReflectorServer{}
+
+type ExternalReflectorServer struct {
+	// TODO define parts here
+}
+
+func NewReflectorExternalServer() *ExternalReflectorServer {
+	return &ExternalReflectorServer{
+	}
+}
+
+// Get is used to retrieve data from a running gRPC service.
+func (t ExternalReflectorServer) Get(ctx context.Context, requestType *RequestType) (*PawsBlob, error) {
+
+	//
+	//
+	// TODO Logic with *PawsBlob
+	//
+	//
+
+
+	response := &PawsBlob{}
+	return response, nil
+}
---
+++ totessafe/reflector/internal_client.go
@@ -0,0 +1,38 @@
+package reflector
+
+import (
+"fmt"
+"github.com/kubicorn/kubicorn/pkg/logger"
+"google.golang.org/grpc"
+)
+
+type InternalReflectorClient struct {
+	hostname string
+	port     int
+	Client   ReflectorInternalClient
+}
+
+
+func NewInternalReflectorClient(hostname string, port int) *InternalReflectorClient {
+	return &InternalReflectorClient{
+		hostname: hostname,
+		port:     port,
+	}
+}
+
+func (c *InternalReflectorClient) dialable() string {
+	return fmt.Sprintf("%s:%d", c.hostname, c.port)
+}
+
+func (c *InternalReflectorClient) Connect() error {
+	logger.Info("Connecting to gRPC server [%s:%d]", c.hostname, c.port)
+	connection, err := grpc.Dial(c.dialable(), grpc.WithInsecure())
+	if err != nil {
+		return fmt.Errorf("Unable to connect to host [%s] with error message: %v", c.dialable(), err)
+	}
+	//defer connection.Close()
+	client := NewReflectorInternalClient(connection)
+	c.Client = client
+	return nil
+}
+
