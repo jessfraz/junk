7b35f65cb9b0d1732c9d9a6bd1e7f88f7e4f5018
Jess Frazelle <acidburn@microsoft.com>
2018-04-02 13:05:47 -0400

    add initial boilerplate
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ version.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/jessfraz/k8s-aks-dns-ingress/version"
+)
+
+const versionHelp = `Show the version information.`
+
+func (cmd *versionCommand) Name() string      { return "version" }
+func (cmd *versionCommand) Args() string      { return "" }
+func (cmd *versionCommand) ShortHelp() string { return versionHelp }
+func (cmd *versionCommand) LongHelp() string  { return versionHelp }
+func (cmd *versionCommand) Hidden() bool      { return false }
+
+func (cmd *versionCommand) Register(fs *flag.FlagSet) {}
+
+type versionCommand struct{}
+
+func (cmd *versionCommand) Run(args []string) error {
+	fmt.Printf(`%s:
+ version     : %s
+ git hash    : %s
+ go version  : %s
+ go compiler : %s
+ platform    : %s/%s
+`, os.Args[0], version.VERSION, version.GITCOMMIT,
+		runtime.Version(), runtime.Compiler, runtime.GOOS, runtime.GOARCH)
+	return nil
+}
